bcc86b8825c8ee351c893624dd16fed1cf0d497c
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-12-31 21:46:39 -0500

    completed change for when player wins

--- script.js
+++ script.js
@@ -16,43 +16,29 @@ const btnHoldEl = document.querySelector('.btn--hold');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
-// score0El.textContent = scores[0];
-// score1El.textContent = scores[1];
-// currentScore1El.textContent = 0;
-// currentScore0El.textContent = 0;
-// diceEl.classList.add('hidden');
 newGame();
 
 // helper functions
-function newGame() {
-  scores[0] = 0;
-  scores[1] = 0;
-  currentScore = 0;
-  activePlayer = 0;
-  score0El.textContent = scores[0];
-  score1El.textContent = scores[1];
-  currentScore1El.textContent = 0;
-  currentScore0El.textContent = 0;
-  diceEl.classList.add('hidden');
-}
-
 function switchPlayer() {
   currentScore = 0;
   if (activePlayer === 0) {
     currentScore0El.textContent = currentScore;
-    player1El.classList.toggle('player--active');
-    player0El.classList.toggle('player--active');
+    player1El.classList.add('player--active');
+    player0El.classList.remove('player--active');
     activePlayer = 1;
   } else {
     currentScore1El.textContent = currentScore;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
     activePlayer = 0;
   }
 }
 
 // handler functions
 function diceRoll() {
+  if (scores[0] >= 100 || scores[1] >= 100) {
+    return;
+  }
   // get a random roll between 1 - 6
   let roll = Math.trunc(Math.random() * 6) + 1;
 
@@ -73,10 +59,16 @@ function diceRoll() {
 }
 
 function holdScore() {
+  if (scores[0] >= 100 || scores[1] >= 100) {
+    return;
+  }
   if (activePlayer === 0) {
     scores[0] += currentScore;
     score0El.textContent = scores[0];
     if (scores[0] >= 100) {
+      player0El.classList.add('player--winner');
+      player0El.classList.remove('player--active');
+      diceEl.classList.add('hidden');
     } else {
       switchPlayer();
     }
@@ -84,9 +76,32 @@ function holdScore() {
     scores[1] += currentScore;
     score1El.textContent = scores[1];
     if (scores[1] >= 100) {
+      player1El.classList.add('player--winner');
+      player1El.classList.remove('player--active');
+      diceEl.classList.add('hidden');
     } else {
       switchPlayer();
     }
+  }
+}
+
+function newGame() {
+  scores[0] = 0;
+  scores[1] = 0;
+  currentScore = 0;
+  activePlayer = 0;
+  score0El.textContent = scores[0];
+  score1El.textContent = scores[1];
+  currentScore1El.textContent = 0;
+  currentScore0El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.add('player--active');
+  if (player0El.classList.contains('player--winner')) {
+    player0El.classList.remove('player--winner');
+  }
+
+  if (player1El.classList.contains('player--winner')) {
+    player1El.classList.remove('player--winner');
   }
 }
 
